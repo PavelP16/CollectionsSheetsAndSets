ace35865393c4f72f3d8a94641dd481db1bc7a65
Pavel <pavelpulle1@gmail.co>
2024-07-23 22:29:05 +0300

    initial commit

---
+++ spring1/src/main/java/com/example/spring1/CollectionsSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsSpring1Application.class, args);
+	}
+
+}
